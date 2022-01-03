cfd27e331614ac633e4ea251d9f5c14bd20e94ec
marcel.medina <marcel.medina@myfoodbag.co.nz>
2022-01-04 00:17:05 +1300

    azure function dependency injection in-process

---
+++ 2021-12-21/DependencyInjectionFunctionSample/DependencyInjectionService/EmailService.cs
@@ -0,0 +1,15 @@
+﻿namespace DependencyInjectionService
+{
+    public interface IEmailService
+    {
+        void SendEmail(string emailTo, string subject, string body);
+    }
+
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(string emailTo, string subject, string body)
+        {
+            // TODO: Send Email
+        }
+    }
+}
