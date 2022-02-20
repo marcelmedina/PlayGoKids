8ca60d842d01ee9001c31fcea06008553e846923
marcel.medina <marcel.medina@myfoodbag.co.nz>
2022-02-21 02:16:40 +1300

    open-telemetry-console-exporter

---
+++ 2022-02-20/OpenTelemetryExporter/OpenTelemetryExporter/MyConsoleExporter.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using OpenTelemetry;
+
+namespace OpenTelemetryExporter
+{
+    class MyConsoleExporter : BaseExporter<Activity>
+    {
+        private readonly string _name;
+
+        public MyConsoleExporter(string name = "MyConsoleExporter")
+        {
+            this._name = name;
+        }
+
+        public override ExportResult Export(in Batch<Activity> batch)
+        {
+            // SuppressInstrumentationScope should be used to prevent exporter
+            // code from generating telemetry and causing live-loop.
+            using var scope = SuppressInstrumentationScope.Begin();
+
+            foreach (var activity in batch)
+            {
+                ConsoleWriteData($"{activity.StartTimeUtc:o}");
+                ConsoleWriteData($"{activity.Status}");
+                ConsoleWriteData("NAME", activity.DisplayName);
+                ConsoleWriteData("TRACE-ID", activity.TraceId.ToString());
+                ConsoleWriteData("BAGGAGE", JsonSerializer.Serialize(activity.Baggage));
+
+                foreach (var ev in activity.Events)
+                {
+                    ConsoleWriteData("EVENT", $"{ev.Name}:{JsonSerializer.Serialize(ev.Tags)}");
+                }
+
+                Console.WriteLine();
+            }
+
+            return ExportResult.Success;
+        }
+
+        private void ConsoleWriteData(string title, string? content = null)
+        {
+            Console.Write(!string.IsNullOrEmpty(content) ? $"[{title} \"{content}\"]" : $"[{title}]");
+        }
+    }
+}
