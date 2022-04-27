8a4a5cc37a99fb15c4d888d9f7e2c6403055230f
marcel.medina <marcel.medina@myfoodbag.co.nz>
2022-04-28 02:07:34 +1200

    model-updates

--- 2022-04-04/FluentValidationSample/FluentValidationSample/Models/ProductViewModel.cs
+++ 2022-04-04/FluentValidationSample/FluentValidationSample/Models/ProductViewModel.cs
@@ -2,12 +2,9 @@
 {
     public class ProductViewModel
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Sku { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
     }
 }
