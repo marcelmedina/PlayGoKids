837578926f0c2a9a49bd8b678b202edb1db42f33
marcel.medina <marcel.medina@myfoodbag.co.nz>
2022-04-28 01:52:32 +1200

    fluentvalidation-tests

--- 2022-04-04/FluentValidationSample/FluentValidationSample/Models/ProductViewModel.cs
+++ 2022-04-04/FluentValidationSample/FluentValidationSample/Models/ProductViewModel.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string Sku { get; set; }
         public int Quantity { get; set; }
+        public double Price { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
     }
--- 2022-04-04/FluentValidationSample/FluentValidationSample/Models/ProductViewModelValidator.cs
+++ 2022-04-04/FluentValidationSample/FluentValidationSample/Models/ProductViewModelValidator.cs
@@ -6,15 +6,11 @@ namespace FluentValidationSample.Models
     {
         public ProductViewModelValidator()
         {
-            When(model => model == null, () =>
-            {
-                RuleFor(model => model).NotNull();
-            }).Otherwise(() =>
-            {
-                RuleFor(model => model.Sku).NotNull();
-                RuleFor(model => model.Sku).NotEmpty().Length(3, 10);
-                RuleFor(model => model.Quantity).NotEqual(0);
-            });
+            RuleFor(model => model.Name).NotNull().NotEmpty().WithMessage("Please specify a name");
+            RuleFor(model => model.Sku).NotNull().NotEmpty().Length(3, 10);
+            RuleFor(model => model.Quantity).GreaterThanOrEqualTo(0);
+            RuleFor(model => model.Price).NotEqual(0).When(model => model.Quantity > 0)
+                .WithMessage("Please specify a price");
         }
     }
 }
