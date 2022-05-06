746a9165e61f70c34256991c5f209b8e4051e122
marcel.medina <marcel.medina@myfoodbag.co.nz>
2022-05-07 02:04:44 +1200

    interface-updates

--- 2021-12-21/DependencyInjectionFunctionSample/DependencyInjectionService/EmailService.cs
+++ 2021-12-21/DependencyInjectionFunctionSample/DependencyInjectionService/EmailService.cs
@@ -1,10 +1,5 @@
 ﻿namespace DependencyInjectionService
 {
-    public interface IEmailService
-    {
-        void SendEmail(string emailTo, string subject, string body);
-    }
-
     public class EmailService : IEmailService
     {
         public void SendEmail(string emailTo, string subject, string body)
--- 2021-12-21/DependencyInjectionFunctionSample/DependencyInjectionService/IEmailService.cs
+++ 2021-12-21/DependencyInjectionFunctionSample/DependencyInjectionService/IEmailService.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DependencyInjectionService
+﻿namespace DependencyInjectionService
 {
-    //public interface IEmailService
-    //{
-    //    void SendEmail(string emailTo, string subject, string body);
-    //}
+    public interface IEmailService
+    {
+        void SendEmail(string emailTo, string subject, string body);
+    }
 }
