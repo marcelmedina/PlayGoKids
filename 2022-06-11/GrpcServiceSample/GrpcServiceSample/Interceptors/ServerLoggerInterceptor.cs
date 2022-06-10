7470d0f1d4a321e7881057975ecc2093a1239810
marcel.medina <marcel.medina@myfoodbag.co.nz>
2022-06-11 02:33:28 +1200

    grpc-sample-with-interceptor

---
+++ 2022-06-11/GrpcServiceSample/GrpcServiceSample/Interceptors/ServerLoggerInterceptor.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+
+namespace GrpcServiceSample.Interceptors
+{
+    public class ServerLoggerInterceptor : Interceptor
+    {
+        private readonly ILogger _logger;
+
+        public ServerLoggerInterceptor(ILogger<ServerLoggerInterceptor> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
+            TRequest request,
+            ServerCallContext context,
+            UnaryServerMethod<TRequest, TResponse> continuation)
+        {
+            _logger.LogInformation($"Starting receiving call. Type: {MethodType.Unary}. " +
+                                   $"Method: {context.Method}.");
+            try
+            {
+                return await continuation(request, context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error thrown by {context.Method}.");
+                throw;
+            }
+        }
+    }
+}
---
+++ 2022-06-11/GrpcServiceSample/GrpcServiceSample/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+
+namespace GrpcServiceSample.Services
+{
+    public class UserService : GrpcServiceSample.UserService.UserServiceBase
+    {
+        private readonly ILogger<UserService> _logger;
+        public UserService(ILogger<UserService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<UserResponse> GetUserDetails(UserRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new UserResponse
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                Address = new Address()
+                {
+                    Number = "1A",
+                    StreetName = "John Smith Street",
+                    Suburb = "Some suburb",
+                    City = "The city",
+                    Country = "Some country"
+                }
+            });
+        }
+    }
+}
