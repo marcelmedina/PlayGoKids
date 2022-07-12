98e9d0e8602db0a61dee16348df6a3478454772b
marcel.medina <marcel.medina@myfoodbag.co.nz>
2022-07-12 17:20:00 +1200

    fluent-validation-azure-function

---
+++ 2022-07-12/FluentValidationFunction/FluentValidationFunction/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentValidationFunction.Models
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public string Sku { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ 2022-07-12/FluentValidationFunction/FluentValidationFunction/Startup.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.AspNetCore;
+using FluentValidationFunction;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace FluentValidationFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddFluentValidation(conf =>
+            {
+                conf.RegisterValidatorsFromAssembly(typeof(Startup).Assembly);
+            });
+        }
+    }
+}
