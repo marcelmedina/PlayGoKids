97702c723c32bde144074d9a31116d6ea7b78b4a
marcel.medina <marcel.medina@myfoodbag.co.nz>
2022-10-02 01:28:33 +1300

    solid-open-closed-principle-module

--- 2022-08-28/SOLID_OCP/Good/Interfaces/IOrderSaver.cs
+++
@@ -1,9 +0,0 @@
-using SOLID_OCP.Models;
-
-namespace SOLID_OCP
-{
-    public interface IOrderSaver
-    {
-        void Save();
-    }
-}
--- 2022-08-28/SOLID_OCP/Good/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using SOLID_OCP;
-using SOLID_OCP.Models;
-
-var order = new Order(1);
-var validator = new OrderValidator(order);
-var saver = new CosmosDbOrderSaver(order);
-var notifier = new OrderNotifier(order);
-var processor = new OrderProcessor(validator, saver, notifier);
-processor.Process();
